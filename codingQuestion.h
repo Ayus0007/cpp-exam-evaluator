bf46db7fe0806bba8299bb86d3fe34d2ce7fbc90
Ayus0007 <94796780+Ayus0007@users.noreply.github.com>
2026-04-08 07:48:02 +0530

    Update codingQuestion.h
    so idk whats going on in here will come back again on this one

--- codingQuestion.h
+++ codingQuestion.h
@@ -1,1 +1,34 @@
+#ifndef CODINGQUESTION_H
+#define CODINGQUESTION_H
 
+#include "question.h"
+#include <vector>
+
+using namespace std;
+
+class CodingQuestion : public Question
+{
+private:
+    vector<string> keywords;
+    string expectedOutput;
+
+public:
+    
+    CodingQuestion(
+        string text,
+        int marks,
+        string topic,
+        vector<string> keys,
+        string output
+    );
+
+   
+    void display();
+
+    int evaluate(string code);
+
+  
+    int countKeywordMatches(string code);
+};
+
+#endif
