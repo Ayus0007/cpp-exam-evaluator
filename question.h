aba8e617b3c535bdb75baba8a1411e450cfafbe7
Ayus0007 <94796780+Ayus0007@users.noreply.github.com>
2026-04-08 07:46:08 +0530

    Update question.h
    basic setup in question header

--- question.h
+++ question.h
@@ -1,1 +1,45 @@
+#ifndef QUESTION_H
+#define QUESTION_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Question
+{
+protected:
+    string questionText;
+    int totalMarks;
+    string topic;
+
+public:
+
+    Question(string text, int marks, string t)
+    {
+        questionText = text;
+        totalMarks = marks;
+        topic = t;
+    }
+    virtual ~Question()
+    {
+    }
+
+    virtual void display() = 0;
+
+    virtual int evaluate(string code) = 0;
+
+ 
+    int getTotalMarks()
+    {
+        return totalMarks;
+    }
+
+    string getTopic()
+    {
+        return topic;
+    }
+};
+
+#endif
